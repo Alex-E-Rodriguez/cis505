7bad99a019d67ddef90b6765113133160f9d4caa
alex.e.rodriguez <alerodriguez@my365.bellevue.edu>
2025-09-08 10:27:33 -0400

    Assignment asked for printf rather than println

--- Module_1/HelloWorldApp/RodriguezHelloWorld.java
+++ Module_1/HelloWorldApp/RodriguezHelloWorld.java
@@ -1,7 +1,7 @@
 public class RodriguezHelloWorld {
     private static void printName(String name_first, String name_last) 
     {
-        System.out.println(String.format("Hello World from %2$s %1$s.", name_first, name_last));
+        System.out.printf("Hello World from %2$s %1$s.", name_first, name_last);
     }
 
     public static void main(String[] args)
