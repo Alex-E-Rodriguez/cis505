d841f982c9f2256809c8f6c7ab99c9558e06b777
alex.e.rodriguez <alerodriguez@my365.bellevue.edu>
2025-09-08 10:33:56 -0400

    Reformatted according to required standards

--- Module_1/HelloWorldApp/RodriguezHelloWorld.java
+++ Module_1/HelloWorldApp/RodriguezHelloWorld.java
@@ -1,11 +1,25 @@
+/*
+    Liang, Y.D. (2019). Introduction to Java Programming and Data Structures:
+    Comprehensive Version (12th ed.). Pearson Education, Inc.
+    Modified by R. Krasso 2021
+    Additional modifications by A. Rodriguez 2025
+*/
+
 public class RodriguezHelloWorld {
-    private static void printName(String name_first, String name_last) 
+
+    /** 
+     * Function prints the user's first and last name in reverse order.
+     * @param nameFirst String
+     * @param nameLast String
+     * @return prints the formatted string to terminal.
+     */
+    private static void printName(String nameFirst, String nameLast) 
     {
-        System.out.printf("Hello World from %2$s %1$s.", name_first, name_last);
-    }
+        System.out.printf("Hello World from %2$s %1$s.", nameFirst, nameLast);
+    } // end printName
 
     public static void main(String[] args)
     {
-        printName("Alex","Rodriguez");
-    }
-}
+        printName("Alex","Rodriguez"); // Calls printName() with author's name ("Alex" and "Rodriguez")
+    } // end main
+} // end RodriguezHelloWorld
